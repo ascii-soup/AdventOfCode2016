a34184ccdba0fd9f9d9b95e0b364cc16f077cd7a
Nils Luxton <ascii.soup@gmail.com>
2016-12-10 19:51:24 +0000

    Circular Lists should have a starting element and next() should return the next(), not the current.

--- src/main/scala/com/asciisoup/advent/DayOne/CircularList.scala
+++ src/main/scala/com/asciisoup/advent/DayOne/CircularList.scala
@@ -6,18 +6,17 @@
 
 package com.asciisoup.advent.DayOne
 
-case class CircularList[T](list: List[T]) {
+case class CircularList[T](list: List[T], startAt: T) {
 
-  private var index: Int = 0
+  private var index: Int = list.indexOf(startAt)
 
   def next: T = {
-    if (index + 1 >= list.length) {
-      index = index % list.length
+    if (index + 1 == list.length) {
+      index = -1
     }
 
-    val x = list(index)
     index += 1
-    x
+    list(index)
   }
 
   def prev: T = {
--- src/test/scala/com/asciisoup/test/advent/DayOne/CircularListTest.scala
+++ src/test/scala/com/asciisoup/test/advent/DayOne/CircularListTest.scala
@@ -12,29 +12,33 @@ import org.scalatest.{FlatSpec, Matchers}
 class CircularListTest extends FlatSpec with Matchers {
 
   "A Circular List" should "give you the next item" in {
-    val list = CircularList(List(1, 2, 3))
-    list.next should be (1)
+    val list = CircularList(List(1, 2, 3), 1)
+    list.next should be (2)
   }
 
   "A Circular List" should "give you the previous item" in {
-    val list = CircularList(List(1, 2, 3))
+    val list = CircularList(List(1, 2, 3), 1)
     list.prev should be (3)
   }
 
   "A Circular List" should "wrap when it reaches the end" in {
-    val list = CircularList(List(1, 2, 3))
-    list.next should be (1)
+    val list = CircularList(List(1, 2, 3), 1)
     list.next should be (2)
     list.next should be (3)
     list.next should be (1)
   }
 
   "A Circular List" should "wrap when it reaches the beginning" in {
-    val list = CircularList(List(1, 2, 3))
+    val list = CircularList(List(1, 2, 3), 1)
     list.prev should be (3)
     list.prev should be (2)
     list.prev should be (1)
     list.prev should be (3)
+  }
+
+  "A Circular List" should "start at the given element" in {
+    val list = CircularList(List(1, 2, 3), 2)
+    list.next should be (3)
   }
 
 }
