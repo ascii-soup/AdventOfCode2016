f5e06f10fbe65f615c63df6fda92ec654985be84
Nils Luxton <ascii.soup@gmail.com>
2016-12-11 18:32:30 +0000

    Day Six Puzzle One + Two complete

---
+++ src/main/scala/com/asciisoup/advent/DaySix/Puzzle.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright ASCII Soup (Nils Luxton) (c) 2016.
+ *
+ * GNU GPL v3 - See LICENSE.txt for details
+ */
+
+package com.asciisoup.advent.DaySix
+
+import scala.io.Source
+
+object Puzzle extends App {
+  val input = Source.fromInputStream(getClass.getResourceAsStream("/day_six.txt"))
+
+  println(
+    input.getLines()
+        .map(s => s.toList) // read line into a list of rows
+        .toSeq
+        .transpose // transpose to a list of columns
+        .map(xs => {
+          xs.groupBy(identity) // chunk the list into char occurrences
+            .mapValues(_.size) // create (Char -> Occurrences) map
+            .toSeq
+            .sortWith(_._2 > _._2) // put it in descending order
+            .map(c => c._1) // make it a list of just the chars
+            .head // pop off the first (most frequent) char
+        })
+        .mkString // stitch the whole lot together into a string
+  )
+
+  input.close()
+}
